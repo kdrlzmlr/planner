0f020f23a7341c2301b376cafeed8e7381ff7da8
kdrlzmlr <kadirluzumlar@gmail.com>
2020-01-16 01:31:15 +0100

    python mongodb work

--- Resource/addResourceMongo.py
+++ Resource/addResourceMongo.py
@@ -2,4 +2,7 @@ import pymongo
 
 myclient = pymongo.MongoClient('mongodb://localhost:27017/')
 
+myDb = myclient.get_database("planner")
+
 print(myclient.list_database_names())
+print(myDb.list_collection_names())
